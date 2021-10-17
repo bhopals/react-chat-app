144c614b7bdf014d6f8e5f6935079f3dcb9ff0fe
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-17 15:14:30 -0400

    style(DropDownMenu): refactor css styling

--- src/components/DropDownMenu/index.jsx
+++ src/components/DropDownMenu/index.jsx
@@ -16,13 +16,12 @@ export default function DropDownMenu ({ user = {} }) {
       <div className='down-arrow-icon down-arrow drop-down-menu-trigger' onClick={onClick}></div>  
       <nav ref={dropdownRef} className={`drop-down-menu-items ${isActive ? 'active' : 'inactive'}`} >
         <ul className='user-profile'>
-          <li>{name}</li>
-          <li>Signed in as <b>{username}</b></li>
-          <li>{email}</li>
-          <li>Messages Received: &nbsp;&nbsp;<b><span className='messages-count'>{messages.length}</span></b></li>
-          <li>Status <b><span className='green-dot'></span>Online</b></li>
-          <li>Last Seen on <b><span className='last-seen'></span>{lastSeen}</b></li>
-
+           <li>{name}</li>
+           <li>Signed in as <b>{username}</b></li>
+           <li>{email}</li>
+           <li>Messages Received &nbsp;<b><span className='messages-count'>{messages.length}</span></b></li>
+           <li>Status <b><span className='green-dot'></span>Online</b></li>
+           <li>Last Seen on <b><span className='last-seen'></span>{lastSeen}</b></li>
         </ul>
       </nav>
     </div>
