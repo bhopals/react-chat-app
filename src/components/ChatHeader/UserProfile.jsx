2a32520a152e89c16109650a8066d641bec0dfe7
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-16 20:54:01 -0400

    feat(chatHeader): add `ChatHeader` components
    Add ChatHeader Components - UserProfile
    
    #Completes ChatHeader

---
+++ src/components/ChatHeader/UserProfile.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import './index.css'
+// import { useSelector } from 'react-redux';
+
+// const UserProfile = () => {
+  
+//   const chatUsers = useSelector(state => state.chat.loggedI);
+
+//   return (
+//         <div className="chat-header">
+//             <div className='left'>
+//                 <span className='close' />
+//                 <span className='minimize' />
+//                 <span className='maximize' />
+//             </div>
+//             <div className='right'>
+//                 <div className='user-profile'>
+//                     <div className='user-avatar'><img src='https://avatars.githubusercontent.com/u/17331876?s=400&u=09765a4e69d46ba6b92c81673b3a4e06be110bc6&v=4' /></div>
+//                     <div className='user-name'>Bhopal Singh</div>
+//                     <div className='down-arrow-icon'></div>
+//                 </div>
+//             </div>
+//         </div>
+//   )
+// }
+
+// // ThemeSelector.propTypes = {
+// // }
+
+// export default UserProfile;
---
+++ src/components/ChatHeader/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+import './index.css'
+
+const ChatHeader = () => {
+  const user = useSelector(state => state.chat.loggedInUser);
+  console.log('ChatHeader>user', user)
+  return (
+    <div className="chat-header">
+        <div className='left'>
+            <span className='close' />
+            <span className='minimize' />
+            <span className='maximize' />
+        </div>
+        <div className='right'>
+            <div className='user-profile'>
+                <div className='user-avatar'><img src={user.profilePhoto} /></div>
+                <div className='user-name'>{user.name}</div>
+                <div className='down-arrow-icon'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatHeader;
