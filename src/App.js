e0530af30e99f4df0c7ad888bf69f585c023a885
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-10-17 12:54:59 -0400

    refactor(ChatAppContainer): refactor jsx and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+
 import {store} from './redux/store';
 import ChatAppContainer from "./components/ChatAppContainer"
 
--- src/components/ChatAppContainer/index.jsx
+++ src/components/ChatAppContainer/index.jsx
@@ -1,63 +1,27 @@
 import React from 'react'
-import ChatService from './../../service/ChatService.js'
 
 import ChatBody from './../ChatBody'
 import ChatHeader from './../ChatHeader'
 import ChatSideBar from './../ChatSideBar'
 
-import './style.css'
+import './index.css'
 
-export default class ChatAppContainer extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      users: [],
-      selectedUser: {}
-    }
-    // this.handleVisualizations = this.handleVisualizations.bind(this)
-  }
-
-
-  // shouldComponentUpdate (nextProps, nextState) {
-  //   if (nextState.isQApiConnected !== this.state.isQApiConnected) {
-  //     return true
-  //   }
-  //   return false
-  // }
-
-  // componentDidUpdate (prevProps, prevState, snapshot) {
-  //   const users = getUsers()
-  //   console.log('componentDidUpdate>', getUsers())
-  //   this.setState({ users, selectedUser: users[0] })
-  // }
-
-  componentDidMount () {
-    const users = ChatService.getUsers()
-    this.setState({ users, selectedUser: users[0] })
-    console.log('componentDidMount>', users)
-  }
-
-
-  render () {
-    const { users, selectedUser } = this.state
-    console.log('ChatAppContainer>users>', users)
-    console.log('ChatAppContainer>this.state>', this.state)
-    return (
-      <div className='chat-app-main'>
-        <div className='chat-app-container'>
+export default function ChatAppContainer () {
+  return (
+    <React.Fragment>
+      <div className='chat-app-container'>
           <div className='header-container'>
-            <ChatHeader user={selectedUser} />
+            <ChatHeader />
           </div>
           <div className='body-container'>
             <div className='panel left-panel'>
-              <ChatSideBar users={users} />
+              <ChatSideBar />
             </div>
             <div className='panel right-panel'>
-              <ChatBody user={selectedUser} />
+              <ChatBody />
             </div>
           </div>
         </div>
-      </div>
-    )
-  }
+    </React.Fragment>
+  )
 }
